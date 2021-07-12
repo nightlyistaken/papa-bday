53fdbbba76178fa4830b7dce21b9e1bb8f480745
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-12 22:45:55 +0530

    V1.2

---
+++ playlist.js
@@ -0,0 +1,19 @@
+// add songs here
+    // config
+
+const playlist = [
+      "Phoolon Ki Tarah.mp3",
+      " Benam sa ye Dard.mp3",
+      " Pyaar Ka Pehla Khat.mp3",
+      "Mujhse Bichhad Ke Khush  .mp3",
+      "Tere Khushboo Mein Base Khat .mp3",
+      "Yeh Mojeza Bhi Mohabbat .mp3 ",
+      " Teri aankhon mein hamne .mp3",
+      "Kahan tum chale gaye.mp3",
+      "Zara Ahista Chal .mp3",
+      "Chori - Chori.mp3",
+
+
+
+      
+    ];
